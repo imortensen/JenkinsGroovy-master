061c91d71b0f90b3e4e442327b98d838842e2bb3
isaac.mortensen <isaac.mortensen@workday.com>
2020-05-04 16:12:16 -0600

    building out jenkinsfile

---
+++ vars/releasenotes.groovy
@@ -0,0 +1,21 @@
+import groovy.io.*;
+
+def call(Map config = [:]){
+    //def dir = new File("/Users/isaac.mortensen/pluralsight/JenkinsGroovy-master/ConsoleApp1");
+    def dir = new File(pwd());
+
+    new File(dir.path + '/releasenotes.txt').withWriter('utf-8')
+    {
+        writer ->
+            dir.eachFileRecurse(FileType.ANY) { file ->
+                if (file.isDirectory()) {
+                    writer.writeLine(file.name);
+                }
+                else
+                {
+                    writer.writeLine('\t' + file.name + '\t' + file.length());
+                }
+
+            }
+    }
+}
